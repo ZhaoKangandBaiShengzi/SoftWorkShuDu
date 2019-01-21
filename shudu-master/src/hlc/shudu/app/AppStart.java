a985aaea52e3f9b0b7d60ec7b42f7e130c0e9159
zhaokang2018520319 <1054920300@qq.com>
2019-01-21 18:03:19 +0800

    提交文件

---
+++ shudu-master/src/hlc/shudu/app/AppStart.java
@@ -0,0 +1,16 @@
+package hlc.shudu.app;
+
+import hlc.shudu.ui.ShuduMainFrame;
+
+public class AppStart {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ShuduMainFrame mainFrame = new ShuduMainFrame();
+		mainFrame.setVisible(true);
+	}
+
+}
